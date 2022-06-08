df9d32042762dc5e8e71d1693895c0d17b7a2ec6
Ubaydulloh <ubaydulloh1000@gmail.com>
2022-06-09 01:09:19 +0500

    database changed

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -123,10 +123,16 @@ WSGI_APPLICATION = 'devsearch.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-try:
-    from .settings_prod import DATABASES
-except:
-    pass
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'devsearchclonedb',
+        "USER": 'postgres',
+        'PASSWORD': "unknown01",
+        'HOST': 'devsearchclonedb.cf1ea4lyvjya.ap-northeast-2.rds.amazonaws.com',
+        'PORT': 5432,
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
